80d299b6494aacb5defeb84f758be154890d5233
Ismail Ou Khalek <ismailhivi@gmail.com>
2021-02-10 01:06:18 +0100

    updated

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
-const API_Key = "AIzaSyAPXyji7zyhfUM5932TTdkD7BXQT5RMCyY";
+const API_Key = "AIzaSyCKESvwpRjNQa6DDLfMnjIqPimi8CBTR_o";
 const ratingSelector = "yt-formatted-string.style-scope.ytd-toggle-button-renderer";
 const viewsSelector = "span.view-count.style-scope.yt-view-count-renderer";
 const commentsSelectors = "#count > yt-formatted-string";
 const channelStatistics= "https://youtube.googleapis.com/youtube/v3/channels?part=statistics&id=iA9HUcjhKNQ&fields=items.statistics.subscriberCount&key="+API_Key;
 const videoStatistics = "https://youtube.googleapis.com/youtube/v3/videos?part=statistics&id=WzKejwI7J38&fields=items.statistics&key="+API_Key;
 
+var isON = true;
 var isPaused = false;
 var timer;
 
-var link = document.createElement('link');
-link.setAttribute('rel', 'stylesheet');
-link.setAttribute('type', 'text/css');
-link.setAttribute('href', 'https://fonts.googleapis.com/css2?family=Faster+One&display=swap');
-document.head.appendChild(link);
+
+var obj;
+var likesObj;
+var dislikesObj;
+var viewsObj;
+
+
+
+window.addEventListener('load', (event) => {
+    console.log("1 " + document.querySelector(viewsSelector));
+});
+
 
 document.addEventListener("visibilitychange", event => {
     if (document.visibilityState == "visible") {
@@ -20,13 +28,46 @@ document.addEventListener("visibilitychange", event => {
     } else {
       isPaused=true;
     }
-  })
+})
 
+main()
+chrome.runtime.onMessage.addListener((request) => {
+    //const url = request.url;
+    isON = false;
+    console.log(request);
+});
+
+
+function main(){
+    var link = document.createElement('link');
+    link.setAttribute('rel', 'stylesheet');
+    link.setAttribute('type', 'text/css');
+    link.setAttribute('href', 'https://fonts.googleapis.com/css2?family=Exo:ital@1&display=swap');
+    document.head.appendChild(link);
+    /*    likesObj.setAttribute("style", "font-family: 'Exo', sans-serif;font-size:21px;");
+    dislikesObj.setAttribute("style", "font-family: 'Exo', sans-serif;font-size:21px;");
+    viewsObj.setAttribute("style", "font-family: 'Exo', sans-serif;font-size:18px;");
+ */
+    Start();
+}
+
+async function Start(){
+
+    if (getVideoId()!=-1){
+        await initElements();
+        timer = setInterval(async ()=>{
+            if(isON&&!isPaused&&(getVideoId()!=-1)){
+                await updateAll()
+                console.log("updated!!")
+        }
+    }, 10000)
+    }
+    timer;
+}
 
 function StringWithCommasToInt(str){
     return parseInt(str.replace(/,/g,""))
 }
-
 
 function animateValue(obj, start, end, duration) {
     start = parseInt(start);
@@ -44,6 +85,24 @@ function animateValue(obj, start, end, duration) {
     };
     window.requestAnimationFrame(step);
   }
+
+function animateValue2(obj, start, end, duration) {
+    start = parseInt(start);
+    if (start=="NaN"){
+        start = 0;
+    }
+    let startTimestamp = null;
+    const step = (timestamp) => {
+      if (!startTimestamp) startTimestamp = timestamp;
+      const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+      obj.innerHTML = Math.floor(progress * (end - start) + start).toLocaleString()+" views";
+      if (progress < 1) {
+        window.requestAnimationFrame(step);
+      }
+    };
+    window.requestAnimationFrame(step);
+  }
+
   
 function timeCalculator(diff){
     if (diff==0) return 10;
@@ -58,7 +117,7 @@ function timeCalculator(diff){
 async function getData(videoId){
     let url = "https://youtube.googleapis.com/youtube/v3/videos?part=statistics&id=" + videoId + "&fields=items.statistics&key=AIzaSyAPXyji7zyhfUM5932TTdkD7BXQT5RMCyY";
     return await fetch(url)
-      .then(async data => {
+    .then(async data => {
         Data1 = await data.json();
         return Data1.items[0].statistics;
     }).catch(error => {
@@ -76,44 +135,31 @@ function getVideoId(){
 async function initElements(){
     let VideoId = getVideoId();
     let data = await getData(VideoId);
-    let obj = document.querySelectorAll(ratingSelector);
-    document.querySelector(viewsSelector).textContent = data.viewCount;
-    obj[0].textContent = data.likeCount;
-    obj[1].textContent = data.dislikeCount;
+    obj = document.querySelectorAll(ratingSelector);
+    likesObj = obj[0];
+    dislikesObj = obj[1];
+    viewsObj = document.querySelector(viewsSelector);
+    likesObj.setAttribute("style", "font-family: 'Exo', sans-serif;");
+    dislikesObj.setAttribute("style", "font-family: 'Exo', sans-serif;");
+    viewsObj.setAttribute("style", "font-family: 'Exo', sans-serif;");
+
+    viewsObj.textContent = data.viewCount.toLocaleString() ;
+    likesObj.textContent = data.likeCount.toLocaleString() ;
+    dislikesObj.textContent = data.dislikeCount.toLocaleString() ;
 }
 
 async function updateAll(){
+    obj = document.querySelectorAll(ratingSelector);
+    likesObj = obj[0];
+    dislikesObj = obj[1];
+    viewsObj = document.querySelector(viewsSelector);
     let data =await getData(getVideoId());
-    let obj = document.querySelectorAll(ratingSelector);
-    let likesObj = obj[0];
-    let dislikesObj = obj[1];
-    let viewsObj = document.querySelector(viewsSelector);
-
     animateValue(likesObj,StringWithCommasToInt(likesObj.textContent), data.likeCount, timeCalculator(data.likeCount-StringWithCommasToInt(likesObj.textContent)));
     animateValue(dislikesObj,StringWithCommasToInt(dislikesObj.textContent), data.dislikeCount, timeCalculator(data.dislikeCount-StringWithCommasToInt(dislikesObj.textContent)));
-    animateValue(viewsObj,StringWithCommasToInt(viewsObj.textContent), data.viewCount, timeCalculator(data.viewCount-StringWithCommasToInt(viewsObj.textContent)));
-}
-
-function pause(){
-    isPaused=true;
-}
-function resume(){
-    isPaused=false;
+    animateValue2(viewsObj,StringWithCommasToInt(viewsObj.textContent), data.viewCount, timeCalculator(data.viewCount-StringWithCommasToInt(viewsObj.textContent)));
 }
 
 function killTimer(){
     clearInterval(timer);
 }
 
-async function Start(){
-    if (getVideoId()!=-1){
-        await initElements();
-        timer = setInterval(async ()=>{
-            if(!isPaused&&(getVideoId()!=-1)){
-                await updateAll()
-                console.log("updated!!")
-        }
-    }, 10000)
-    }
-}
-Start();
